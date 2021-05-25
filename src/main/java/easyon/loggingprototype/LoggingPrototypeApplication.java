989c5f99692c70b73ac7247dde65be0cdd887bdb
MatiasStorm <mat-435@hotmail.com>
2021-05-25 14:44:41 +0200

    Initial commit

---
+++ src/main/java/easyon/loggingprototype/LoggingPrototypeApplication.java
@@ -0,0 +1,13 @@
+package easyon.loggingprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingPrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/easyon/loggingprototype/LoggingPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package easyon.loggingprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
